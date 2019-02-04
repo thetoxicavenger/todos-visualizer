1b3eeec37a9f4897bac9627c31dc2f73d84afcf5
Mitch Cravens <mcravens12@gmail.com>
2019-02-04 16:22:13 -0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace visualize_todos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
